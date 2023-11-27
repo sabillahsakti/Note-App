6d46dd42b086963f97a8bc471e4940b84919c593
Sabillah Sakti <sabillahsakti@Sabillahs-MacBook-Air.local>
2023-11-27 21:46:53 +0700

    fix function click category

--- src/components/kecil/pilihan/index.js
+++ src/components/kecil/pilihan/index.js
@@ -1,55 +1,65 @@
-import { Text, Select, SelectBackdrop, SelectContent, SelectDragIndicator, SelectDragIndicatorWrapper, SelectInput, SelectItem, SelectPortal, SelectTrigger, FormControlLabel } from "@gluestack-ui/themed";
-import React, { useState } from "react";
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Picker } from '@react-native-picker/picker';
 
 const Pilihan = ({ label, datas, width, height, fontSize, selectedValue, onValueChange }) => {
-  if (label === "Status") {
-    return (
-      <>
-        <FormControlLabel marginTop={"$10"}>
-          <Text fontSize={fontSize ? fontSize : "$lg"}>{label} :</Text>
-        </FormControlLabel>
-        <Select onValueChange={onValueChange} selectedValue={selectedValue}>
-          <SelectTrigger>
-            <SelectInput fontSize={fontSize ? fontSize : "$lg"} placeholder="-- Pilih ---" width={width} height={height ? height : "$40"} color="$black" />
-          </SelectTrigger>
-          <SelectPortal>
-            <SelectBackdrop />
-            <SelectContent>
-              <SelectDragIndicatorWrapper borderWidth={"$1"} borderRadius={"$sm"}>
-                <SelectDragIndicator />
-              </SelectDragIndicatorWrapper>
-              <SelectItem label="Progress" value="progress" />
-              <SelectItem label="Done" value="done" />
-            </SelectContent>
-          </SelectPortal>
-        </Select>
-      </>
-    );
-  } else if (label === "Category") {
-    return (
-      <>
-        <FormControlLabel marginTop={"$10"}>
-          <Text fontSize={fontSize ? fontSize : "$lg"}>{label} :</Text>
-        </FormControlLabel>
-        <Select onValueChange={onValueChange} selectedValue={selectedValue}>
-          <SelectTrigger>
-            <SelectInput fontSize={fontSize ? fontSize : "$lg"} placeholder="-- Pilih ---" width={width} height={height ? height : "$40"} color="$black" />
-          </SelectTrigger>
-          <SelectPortal>
-            <SelectBackdrop />
-            <SelectContent>
-              <SelectDragIndicatorWrapper borderWidth={"$1"} borderRadius={"$sm"}>
-                <SelectDragIndicator />
-              </SelectDragIndicatorWrapper>
-              {datas.map((data, index) => (
-                <SelectItem key={index} label={data} value={data} />
-              ))}
-            </SelectContent>
-          </SelectPortal>
-        </Select>
-      </>
-    );
-  }
-};
 
-export default Pilihan;
+    if (label === "Status") {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.label(fontSize)}>{label} :</Text>
+                <View style={styles.wrapperPicker}>
+                    <Picker
+                        selectedValue={selectedValue}
+                        itemStyle={styles.picker(width, height, fontSize)}
+                        onValueChange={onValueChange}
+                    >
+                        <Picker.Item label="-- Pilih --" value="" />
+                        <Picker.Item label="Progress" value="progress" />
+                        <Picker.Item label="Done" value="done" />
+                    </Picker>
+                </View>
+            </View>
+        )
+    } else if (label === "Category") {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.label(fontSize)}>{label} :</Text>
+                <View style={styles.wrapperPicker}>
+                    <Picker
+                        selectedValue={selectedValue}
+                        itemStyle={styles.picker(width, height, fontSize)}
+                        onValueChange={onValueChange}
+                    >
+                        <Picker.Item label="-- Pilih --" value="" />
+                        {datas.map((data, index) => (
+                            <Picker.Item key={index} label={data} value={data} />
+                        ))}
+                    </Picker>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default Pilihan
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+    },
+
+    label: (fontSize) => ({
+        fontSize: fontSize ? fontSize : 18,
+    }),
+    picker: (width, height, fontSize) => ({
+        fontSize: fontSize ? fontSize : 18,
+        width: width,
+        height: height ? height : 46,
+        color: 'black'
+    }),
+    wrapperPicker: {
+        borderWidth: 1,
+        borderRadius: 5,
+    }
+})
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Box, Text } from "@gluestack-ui/themed";
+import { Box } from "@gluestack-ui/themed";
 import { CategoryTab, ListNote } from "../../components";
 import { getNote } from "../../actions/AuthAction";
 
 const Home = ({ navigation }) => {
   const [userNotes, setUserNotes] = useState([]);
   const [category, setCategory] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const notes = await getNote();
       const categories = notes.map((note) => note.category);
-      const uniqueCategories = categories.filter((value, index, self) => {
-        return self.indexOf(value) === index;
-      });
+      const uniqueCategories = Array.from(new Set(categories));
       setUserNotes(notes);
       setCategory(uniqueCategories);
     };
@@ -25,15 +24,28 @@ const Home = ({ navigation }) => {
     };
   }, [navigation]);
 
+  const onCategoryPress = (selectedCategory) => {
+    setSelectedCategory(selectedCategory);
+  };
+
+  const filteredNotes = selectedCategory
+    ? userNotes.filter((note) => note.category === selectedCategory)
+    : userNotes;
+
   return (
-    <Box marginHorizontal={"$2"} mt={"$10"}>
-      <Box flexDirection="row" marginBottom={"$3"}>
+    <Box marginHorizontal="$2" marginTop="$10">
+      <Box flexDirection="row" marginBottom="$3">
         {category.map((note, index) => (
-          <CategoryTab key={index} title={note} padding={"$2"} />
+          <CategoryTab
+            key={index}
+            title={note}
+            padding="$2"
+            onPress={() => onCategoryPress(note)}
+          />
         ))}
       </Box>
-      <Box flexDirection="row" marginBottom={"$3"}>
-        {userNotes.map((note, index) => (
+      <Box flexDirection="column" marginBottom="$3">
+        {filteredNotes.map((note, index) => (
           <ListNote
             key={index}
             judul={note.title}
